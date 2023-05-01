d7096e6783a9dc6c70edc683d32b2d7c6e5b8bbf
Michaela <michaela.wonacott@gmail.com>
2023-05-01 14:13:35 -0700

    Updated url for pythonanywhere server.

--- mysite/settings.py
+++ mysite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'Iris2.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
